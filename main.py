b88ddc403cd793b600a45c8e6e6e390ae387d54c
Shri2007 <66092664+Shri2007@users.noreply.github.com>
2020-06-07 20:56:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import random
+#This is telling the code this is a randomizer.
 min = 1
+#The minimum of a dice is 1.
 max = 6
+#The maximum of a dice is 6.
 roll_again = "yes"
-
-while roll_again == "yes" or roll_again == "Yes" or roll_again == "y":
+#Variables can also be a string.
+while roll_again == "yes" or roll_again == "Yes" or roll_again == "y" or roll_again == "Y":
+  #The while loop which will loop over and over again.
   print("Rolling the dice...")
   print("The numbers are")
   print(random.randint(min, max))
   print(random.randint(min, max))
+  #This gives a random number
   
   roll_again = input("Want to roll again? ")
-  hi = "no"
-if hi == "no":
-  print("Ok!")
-  print("Bye!")
-else:
- print("Ok!")
-
+  #If you say "Yes", this will continue.
